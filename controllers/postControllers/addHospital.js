7d1f29719317cf2404e0860a94cc4c39d22b854b
miteshp2110 <paliwalmitesh2110@gmail.com>
2024-03-23 02:11:00 +0530

    test5

--- controllers/postControllers/addHospital.js
+++ controllers/postControllers/addHospital.js
@@ -4,17 +4,20 @@ const {pool}=require('../../models/dbConnection')
 
 const addHospital=(async (req,res)=>{
     var values=req.body
-    //console.log(values)
+    console.log(values)
     const query=`INSERT INTO hData(city, hName, hSpecialization, hContact, hEmail, hAddress, hUrl)VALUES ($1,$2,$3,$4,$5,$6,$7)`
     const val=[values.city,values.hName,values.hSpecialization,values.hContact,values.hEmail,values.hAddress,values.hUrl]
+    console.log("before")
     try{
         pool.query(query,val,(err,result)=>{
             
             if(err){
+                console.log(err)
                 return res.status(500).json({'error':'error while in database'})
             }
             
         })
+        console.log("after....")
         return res.status(201).json({'message':'Data added'})
 
     }
